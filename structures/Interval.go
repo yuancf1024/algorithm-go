b547babaf45723929bd9cfe5efbf75f83044b2c3
“yuancf1024” <3455911441@qq.com>
2022-04-03 11:43:10 +0800

    add QuickSort

--- structures/Interval.go
+++ structures/Interval.go
@@ -35,3 +35,25 @@ func Intss2IntervalSlice(intss [][]int) []Interval {
 	return res
 }
 
+// 快速排序QuickSort 的定义
+func QuickSort(a []Interval, lo, hi int) {
+	if lo > hi {
+		return
+	}
+	p := partitionSort(a, lo, hi)
+	QuickSort(a, lo, p-1)
+	QuickSort(a, p+1, hi)
+}
+
+func partitionSort(a []Interval, lo, hi int) int {
+	pivot := a[hi]
+	i := lo - 1
+	for j := lo; j < hi; j++ {
+		if (a[j].Start < pivot.Start) || (a[j].Start == pivot.Start && a[j].End < pivot.End) {
+			i++
+			a[j], a[i] = a[i], a[j]
+		}
+	}
+	a[i+1], a[hi] = a[hi], a[i+1]
+	return i+1
+}
