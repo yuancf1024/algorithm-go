40056be04390d762b3d318a8decdc7d1f0766027
“yuancf1024” <3455911441@qq.com>
2022-04-03 11:16:48 +0800

    2022-04-03 add Interval

---
+++ structures/Interval.go
@@ -0,0 +1,37 @@
+package structures
+
+// Interval 提供区间表示
+type Interval struct {
+	Start int
+	End int
+}
+
+// Interval2Ints 把 Interval 转换成 整型切片
+func Interval2Ints(i Interval) []int {
+	return []int{i.Start, i.End}
+}
+
+// IntervalSlice2Intss 把 []Interval 转换成 [][]int
+func IntervalSlice2Intss(is []Interval) [][]int {
+	res := make([][]int, 0, len(is))
+	for i := range is {
+		res = append(res, Interval2Ints(is[i]))
+	}
+	return res
+}
+
+/*
+区间切片和二维切片的区别
+区间切片：[{1 2} {3 4}]
+二维切片：[[1 2] [3 4]]
+*/
+
+// Intss2IntervalSlice 把 [][]int 转换成 []Interval
+func Intss2IntervalSlice(intss [][]int) []Interval {
+	res := make([]Interval, 0, len(intss))
+	for _, ints := range intss {
+		res = append(res, Interval{Start: ints[0], End: ints[1]})
+	}
+	return res
+}
+
