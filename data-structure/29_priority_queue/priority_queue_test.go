b63bdda5a85857397f400a82515d3ff3ff1602ba
“yuancf1024” <3455911441@qq.com>
2022-04-06 22:19:11 +0800

    注释掉失败测试

--- data-structure/29_priority_queue/priority_queue_test.go
+++ data-structure/29_priority_queue/priority_queue_test.go
@@ -50,29 +50,29 @@ func Test_PushPop(t *testing.T) {
 }
 
 // 无法保证入队顺序和出队顺序的一致性
-func Test_PushPop_Equal(t *testing.T) {
-	queue := NewPriorityQueue(9)
+// func Test_PushPop_Equal(t *testing.T) {
+// 	queue := NewPriorityQueue(9)
 
-	queue.Push(Node{0, 1})  // 8
-	queue.Push(Node{3, 1})  // 9
-	queue.Push(Node{3, 2})  // 3
-	queue.Push(Node{6, 2})  // 4
-	queue.Push(Node{11, 3}) // 2
-	queue.Push(Node{12, 2}) // 5
-	queue.Push(Node{13, 2}) // 6
-	queue.Push(Node{19, 5}) // 1
-	queue.Push(Node{17, 2}) // 7
+// 	queue.Push(Node{0, 1})  // 8
+// 	queue.Push(Node{3, 1})  // 9
+// 	queue.Push(Node{3, 2})  // 3
+// 	queue.Push(Node{6, 2})  // 4
+// 	queue.Push(Node{11, 3}) // 2
+// 	queue.Push(Node{12, 2}) // 5
+// 	queue.Push(Node{13, 2}) // 6
+// 	queue.Push(Node{19, 5}) // 1
+// 	queue.Push(Node{17, 2}) // 7
 
-	assert.Equal(t, 19, queue.Pop().value)
-	assert.Equal(t, 11, queue.Pop().value)
-	assert.Equal(t, 3, queue.Pop().value)
-	// 此时出队的是 3, 6, 12, 13, 17 中的任意一个
-	// 无法保证入队顺序和出队顺序的一致性
+// 	assert.Equal(t, 19, queue.Pop().value)
+// 	assert.Equal(t, 11, queue.Pop().value)
+// 	assert.Equal(t, 3, queue.Pop().value)
+// 	// 此时出队的是 3, 6, 12, 13, 17 中的任意一个
+// 	// 无法保证入队顺序和出队顺序的一致性
 
-	assert.Equal(t, 6, queue.Pop().value)
-	assert.Equal(t, 12, queue.Pop().value)
-	assert.Equal(t, 13, queue.Pop().value)
-	assert.Equal(t, 17, queue.Pop().value)
-	assert.Equal(t, 0, queue.Pop().value)
-	assert.Equal(t, 3, queue.Pop().value)
-}
+// 	assert.Equal(t, 6, queue.Pop().value)
+// 	assert.Equal(t, 12, queue.Pop().value)
+// 	assert.Equal(t, 13, queue.Pop().value)
+// 	assert.Equal(t, 17, queue.Pop().value)
+// 	assert.Equal(t, 0, queue.Pop().value)
+// 	assert.Equal(t, 3, queue.Pop().value)
+// }
