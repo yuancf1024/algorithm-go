3cd9841f47f11807a2d3c0fac82354bafa6d9fd7
“yuancf1024” <3455911441@qq.com>
2022-04-11 11:22:19 +0800

    leetcode-119. 杨辉三角 II add空间复杂度为O(k)的解法

--- leetcode/array/119.Pascals-Triangle-II/119.Pascals-Triangle-II.go
+++ leetcode/array/119.Pascals-Triangle-II/119.Pascals-Triangle-II.go
@@ -1,7 +1,13 @@
 package leetcode
 
+/*
+给定一个非负索引 rowIndex，返回「杨辉三角」的第 rowIndex 行。
+
+在「杨辉三角」中，每个数是它左上方和右上方的数的和。
+*/
+
 // 自己写的
-func getRow(rowIndex int) []int {
+func getRow1(rowIndex int) []int {
     res := [][]int{}
     for i := 0; i <= rowIndex; i++ { // 和leetcode118的差异
         row := []int{}
@@ -18,5 +24,26 @@ func getRow(rowIndex int) []int {
     return res[rowIndex]
 }
 
-// 更优雅的解法
+// 更优雅的解法 优化到 O(rowIndex) 空间复杂度
 // TODO
+func getRow(rowIndex int) []int {
+    row := make([]int, rowIndex+1)
+    row[0] = 1
+    for i := 1; i <= rowIndex; i++ {
+        row[i] = row[i-1] * (rowIndex - i + 1) / i
+    }
+    return row
+}
+
+/* 解题思路
+题目中的三角是杨辉三角，每个数字是 (a+b)^n 二项式展开的系数。
+题目要求我们只能使用 O(k) 的空间。那么需要找到两两项直接的递推关系。
+由组合知识得知：
+C_{n}^{m} = \frac{n!} {m!*(n-m)!}
+
+C_{n}^{m-1} = \frac{n!} {(m-1)!*(n-m+1)!}
+于是得到递推公式：
+
+C_{n}^{m} = C_{n}^{m-1} * \frac{n-m+1} {m}
+利用这个递推公式即可以把空间复杂度优化到 O(k)
+*/
