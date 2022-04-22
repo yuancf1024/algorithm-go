fcf977fba428f6cafcfd07ddc18b10088e3be37e
“yuancf1024” <3455911441@qq.com>
2022-04-22 17:23:05 +0800

    add 递归方法

--- leetcode/two-pointers/344.Reverse-String/344.Reverse-String.go
+++ leetcode/two-pointers/344.Reverse-String/344.Reverse-String.go
@@ -24,6 +24,24 @@ func reverseString_v1(s []byte) {
 		j--
 	}
 }
+
+// 递归的实现，还稍微不太好理解
+func reverseString_v2(s []byte) {
+    res := make([]byte, 0)
+    reverse(s, 0, &res)
+    n := len(s)
+    for i := 0; i < n; i++ {
+        s[i] = res[i]
+    }
+}
+
+func reverse(s []byte, i int, res *[]byte) {
+    if i == len(s) {
+        return
+    }
+    reverse(s, i+1, res) // 一直递归，直到找到最后一个元素，然后将其添加进res，再不断返回
+    *res = append(*res, s[i])
+}
 /*
 这一题的解题思路是用 2 个指针，指针对撞的思路，来不断交换首尾元素，即可。
 */
