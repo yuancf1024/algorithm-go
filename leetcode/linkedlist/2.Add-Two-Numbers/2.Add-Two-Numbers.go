f88a1a0bb95dd72c03af6a1f8736475ced59a8f4
“yuancf1024” <3455911441@qq.com>
2022-05-01 12:06:34 +0800

    add 题目说明

--- leetcode/linkedlist/2.Add-Two-Numbers/2.Add-Two-Numbers.go
+++ leetcode/linkedlist/2.Add-Two-Numbers/2.Add-Two-Numbers.go
@@ -14,6 +14,15 @@ type ListNode = structures.ListNode
  * }
  */
 
+/* 2. 两数相加
+给你两个 非空 的链表，表示两个非负的整数。
+它们每位数字都是按照 逆序 的方式存储的，
+并且每个节点只能存储 一位 数字。
+
+请你将两个数相加，并以相同形式返回一个表示和的链表。
+你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+*/
+
 //  精简、巧妙的实现
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	head := &ListNode{Val: 0} // 新建虚拟头结点
