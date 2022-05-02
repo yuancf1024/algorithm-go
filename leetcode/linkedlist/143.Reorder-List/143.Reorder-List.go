3a7636c0597743be5ac21eab8c30ccbb2cca428e
“yuancf1024” <3455911441@qq.com>
2022-05-02 10:39:14 +0800

    leetcode-143. 重排链表 添加注释

--- leetcode/linkedlist/143.Reorder-List/143.Reorder-List.go
+++ leetcode/linkedlist/143.Reorder-List/143.Reorder-List.go
@@ -38,7 +38,7 @@ func reorderList(head *ListNode) *ListNode {
 	}
 	tmp := slow.Next
 	slow.Next = nil
-	link(head, reverseList(tmp), dummy)
+	link(head, reverseList(tmp), dummy) // 此处的哨兵节点处理得很妙
 	return head
 }
 
@@ -60,14 +60,14 @@ func reverseList(head *ListNode) *ListNode {
 func link(node1, node2, head *ListNode) {
 	prev := head
 	for node1 != nil && node2 != nil {
-		tmp := node1.Next
+		tmp := node1.Next // 保存node1后面的节点
 
 		prev.Next = node1
 		node1.Next = node2
-		prev = node2
+		prev = node2 // prev为暂时连接链表的最后一个节点
 
-		node1 = tmp
-		node2 = node2.Next
+		node1 = tmp // node1后移
+		node2 = node2.Next // node2后移
 	}
 	if node1 != nil {
 		prev.Next = node1 // 奇数情况下，前半段比后半段多1个节点
@@ -80,4 +80,59 @@ func link(node1, node2, head *ListNode) {
 - 2. 反转链表
 - 3. 考虑链表节点奇偶数的问题。链表节点为奇数时，要确保链表的前半段
 比后半段多一个节点。
+*/
+
+// 参考halfrost 不是很好理解
+// 解法1：单链表
+func reorderList_v1(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	// 寻找中间节点
+	p1, p2 := head, head
+	for p2.Next != nil && p2.Next.Next != nil {
+		p1 = p1.Next
+		p2 = p2.Next.Next
+	}
+
+	// 反转链表后半部分 1->2->3->4->5->6 to 1->2->3->6->5->4
+	// preMiddle := p1
+	// preCurrent := p1.Next
+	// for preCurrent.Next != nil {
+	// 	current := preCurrent.Next
+		
+	// }
+	preMiddle := p1
+	preCurrent := p1.Next
+	for preCurrent.Next != nil {
+		current := preCurrent.Next
+		preCurrent.Next = current.Next
+		current.Next = preMiddle.Next
+		preMiddle.Next = current
+	}
+
+	// 重新拼接链表  1->2->3->6->5->4 to 1->6->2->5->3->4
+	p1 = head
+	p2 = preMiddle.Next // 后半段链表头节点
+	for p1 != preMiddle {
+		preMiddle.Next = p2.Next
+		p2.Next = p1.Next
+		p1.Next = p2
+		p1 = p2.Next
+		p2 = preMiddle.Next
+	}
+	return head
+}
+/*
+参考halfrost：
+最近简单的方法是先把链表存储到数组里，然后找到链表中间的结点，
+按照规则拼接即可。这样时间复杂度是 O(n)，空间复杂度是 O(n)。
+
+更好的做法是结合之前几道题的操作：链表逆序，找中间结点。
+先找到链表的中间结点，然后利用逆序区间的操作，
+如 第 92 题 里的 reverseBetween() 操作，
+只不过这里的反转区间是从中点一直到末尾。
+最后利用 2 个指针，一个指向头结点，一个指向中间结点，
+开始拼接最终的结果。这种做法的时间复杂度是 O(n)，空间复杂度是 O(1)。
 */
