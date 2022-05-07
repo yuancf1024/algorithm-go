177ebd91f49f649fd742737a90df3734208d1b8c
“yuancf1024” <3455911441@qq.com>
2022-05-07 23:02:41 +0800

    leetcode-695. 岛屿的最大面积 cpp实现+多种方法

--- leetcode-cpp/695.Max-Area-of-Island/695.Max-Area-of-Island.cpp
+++ leetcode-cpp/695.Max-Area-of-Island/695.Max-Area-of-Island.cpp
@@ -104,3 +104,29 @@ public:
 };
 
 // 边界条件的判断放在辅函数第一行
+class Solution {
+public:
+    // 边界条件的判断放在递归搜索函数之前
+    // 主函数
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        if (grid.empty() || grid[0].empty()) return 0;
+        int max_area = 0;
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid[0].size(); ++j) {
+                max_area = max(max_area, dfs(grid, i, j));
+            }
+        }
+        return max_area;
+    }
+
+    // 辅函数
+    int dfs(vector<vector<int>>& grid, int r, int c) {
+        if (r < 0 || r >= grid.size() ||
+        c < 0 || c >= grid[0].size() || grid[r][c] == 0) {
+            return 0;
+        }
+        grid[r][c] = 0;
+        return 1 + dfs(grid, r+1, c) + dfs(grid, r-1, c) + 
+                    dfs(grid, r, c+1) + dfs(grid, r, c-1);
+    }
+};
