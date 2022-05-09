ca7e8c0033078b3f47b957416853ae690e81e9cf
“yuancf1024” <3455911441@qq.com>
2022-05-09 09:44:30 +0800

    add 注释

--- leetcode/DFS/79.Word-Search/79.Word-Search.go
+++ leetcode/DFS/79.Word-Search/79.Word-Search.go
@@ -32,13 +32,13 @@ var dir = [][]int {
 }
 
 func exist(board [][]byte, word string) bool {
-	visited := make([][]bool, len(board))
+	visited := make([][]bool, len(board)) // 记录已经访问过的字符，二维切片visited
 	for i := 0; i < len(visited); i++ {
 		visited[i] = make([]bool, len(board[0]))
 	}
-	for i, v := range board {
+	for i, v := range board { // 遍历字符板board上的每一个字符
 		for j := range v {
-			if searchWord(board, visited, word, 0, i, j) {
+			if searchWord(board, visited, word, 0, i, j) { // DFS搜索目标单词word
 				return true
 			}
 		}
@@ -46,24 +46,27 @@ func exist(board [][]byte, word string) bool {
 	return false
 }
 
+// 判断x、y是否还在board内
 func isInboard(board [][]byte, x, y int) bool {
 	return x >= 0 && x < len(board) && y >= 0 && y < len(board[0])
 }
 
+// 搜索单词
 func searchWord(board [][]byte, visited [][]bool, word string, index, x, y int) bool {
-	if index == len(word) - 1 {
+	if index == len(word) - 1 { // 如果递归到查找的字符是单词的最后一个字符，直接返回判断结果
 		return board[x][y] == word[index]
 	}
 	if board[x][y] == word[index] {
-		visited[x][y] = true
+		visited[x][y] = true // 将x、y标记为已经遍历过
 		for i := 0; i < 4; i++ {
 			nx := x + dir[i][0]
 			ny := y + dir[i][1]
+			// 如果x、y四周的点还在字符网格board内，且没有遍历过，并且搜索对应的index的字符返回ture，那么也返回true
 			if isInboard(board, nx, ny) && !visited[nx][ny] && searchWord(board, visited, word, index+1, nx, ny) {
 				return true
 			}
 		}
-		visited[x][y] = false
+		visited[x][y] = false // 遍历了x、y四周之后，将x、y标记为false，方便后面遍历其他点的时候再遍历到x、y
 	}
 	return false
 }
