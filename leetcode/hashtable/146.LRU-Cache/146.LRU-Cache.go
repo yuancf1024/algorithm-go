ab86d3ef47470255c6543d07df814894ec9a89db
“yuancf1024” <3455911441@qq.com>
2022-05-15 22:38:18 +0800

    leetcode-146. LRU 缓存

---
+++ leetcode/hashtable/146.LRU-Cache/146.LRU-Cache.go
@@ -0,0 +1,121 @@
+package leetcode
+
+/* 146. LRU 缓存
+请你设计并实现一个满足  LRU (最近最少使用) 缓存 约束的数据结构。
+实现 LRUCache 类：
+LRUCache(int capacity) 以 正整数 作为容量 capacity 初始化 LRU 缓存
+int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，
+否则返回 -1 。
+void put(int key, int value) 如果关键字 key 已经存在，则变更其数据值 value ；
+如果不存在，则向缓存中插入该组 key-value 。
+如果插入操作导致关键字数量超过 capacity ，则应该 逐出 最久未使用的关键字。
+函数 get 和 put 必须以 O(1) 的平均时间复杂度运行。
+
+示例：
+
+输入
+["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+[[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+
+输出
+[null, null, null, 1, null, -1, null, -1, 3, 4]
+
+解释
+LRUCache lRUCache = new LRUCache(2);
+lRUCache.put(1, 1); // 缓存是 {1=1}
+lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
+lRUCache.get(1);    // 返回 1
+lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+lRUCache.get(2);    // 返回 -1 (未找到)
+lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+lRUCache.get(1);    // 返回 -1 (未找到)
+lRUCache.get(3);    // 返回 3
+lRUCache.get(4);    // 返回 4
+*/
+
+// 参考halfrost
+type LRUCache struct {
+	head, tail *Node
+	Keys map[int]*Node
+	Cap int
+}
+
+type Node struct {
+	Key, Val int
+	Prev, Next *Node
+}
+
+
+func Constructor(capacity int) LRUCache {
+	return LRUCache{Keys: make(map[int]*Node), Cap: capacity}
+}
+
+
+func (this *LRUCache) Get(key int) int {
+	if node, ok := this.Keys[key]; ok {
+		this.Remove(node)
+		this.Add(node)
+		return node.Val
+	}
+	return -1
+}
+
+
+func (this *LRUCache) Put(key int, value int)  {
+	if node, ok := this.Keys[key]; ok {
+		node.Val = value
+		this.Remove(node)
+		this.Add(node)
+		return
+	} else {
+		node = &Node{Key: key, Val: value}
+		this.Keys[key] = node
+		this.Add(node)
+	}
+	if len(this.Keys) > this.Cap {
+		delete(this.Keys, this.tail.Key)
+		this.Remove(this.tail)
+	}
+}
+
+func (this *LRUCache) Add(node *Node) {
+	node.Prev = nil
+	node.Next = this.head
+	if this.head != nil {
+		this.head.Prev = node
+	}
+	this.head = node
+	if this.tail == nil {
+		this.tail = node
+		this.tail.Next = nil
+	}
+}
+
+func (this *LRUCache) Remove(node *Node) {
+	if node == this.head {
+		this.head = node.Next
+		node.Next = nil
+		return
+	}
+	if node == this.tail {
+		this.tail = node.Prev
+		node.Prev.Next = nil
+		node.Prev = nil
+		return
+	}
+	node.Prev.Next = node.Next
+	node.Next.Prev = node.Prev
+}
+
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * obj := Constructor(capacity);
+ * param_1 := obj.Get(key);
+ * obj.Put(key,value);
+ */
+
+/*
+解题思路
+这一题是 LRU 经典面试题，详细解释见第三章 LRUCache 模板。
+*/
