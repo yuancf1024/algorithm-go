04ca961f65f530d9da3c44f28ef4f5d675531a04
“yuancf1024” <3455911441@qq.com>
2022-05-17 21:50:45 +0800

    测试文件

---
+++ leetcode-cpp/testcpp.cpp
@@ -0,0 +1,11 @@
+// 在画面上输出
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // cout << "第一个C++程序。\n";
+    // cout << "在画面上输出。\n";
+    printf("第一个C++程序。\n");
+    printf("在画面上输出。\n");
+}
