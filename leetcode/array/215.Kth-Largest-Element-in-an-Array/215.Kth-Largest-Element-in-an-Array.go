687214e965d534f49a0a8f63b88a3eaf32198da5
“yuancf1024” <3455911441@qq.com>
2022-07-02 20:33:49 +0800

    add 剑指offer 面试题40. 最小的k个数

--- leetcode/array/215.Kth-Largest-Element-in-an-Array/215.Kth-Largest-Element-in-an-Array.go
+++ leetcode/array/215.Kth-Largest-Element-in-an-Array/215.Kth-Largest-Element-in-an-Array.go
@@ -89,3 +89,47 @@ func partition( nums []int, l, r int) int {
 如果这个元素的下标比 K 大，那么就在左边的区间继续执行 partition 操作；
 如果相等就直接输出这个下标对应的数组元素即可。
 */
+
+/* 面试题40. 最小的k个数
+输入整数数组 arr ，找出其中最小的 k 个数。
+例如，输入4、5、1、6、2、7、3、8这8个数字，
+则最小的4个数字是1、2、3、4。
+ 
+示例 1：
+输入：arr = [3,2,1], k = 2
+输出：[1,2] 或者 [2,1]
+
+示例 2：
+输入：arr = [0,1,2,1], k = 1
+输出：[0]
+ 
+限制：
+0 <= k <= arr.length <= 10000
+0 <= arr[i] <= 10000
+
+*/
+// 扩展题 剑指 Offer 40. 最小的 k 个数
+func getLeastNumbers(arr []int, k int) []int {
+	return selectSmallest1(arr, 0, len(arr)-1, k)[:k]
+}
+
+// 和 selectSmallest 实现完全一致，只是返回值不用再截取了，直接返回 nums 即可
+func selectSmallest1(nums []int, l, r, i int) []int {
+	if l >= r {
+		return nums
+	}
+	q := partition(nums, l, r)
+	k := q - l + 1 // 转化一下
+	if k == i {
+		return nums
+	}
+	if i < k { // k是partition后的元素，i是原题中的k
+		// 执行一次 partition 操作以后，元素下标比 K 大，在左边的区间继续执行 partition 操作
+		return selectSmallest1(nums, l, q-1, i)
+	} else {
+		// 执行一次 partition 操作以后，元素下标比 K 小，在后边的区间继续执行 partition 操作
+		return selectSmallest1(nums, q+1, r, i-k)
+	}
+}
+
+// partition 函数同上
