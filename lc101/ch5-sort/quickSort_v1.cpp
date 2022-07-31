4a0788607d4e81d7b5fe06bba426e50542885dca
“yuancf1024” <3455911441@qq.com>
2022-07-31 09:35:00 +0800

    修改快排版本细节

--- lc101/ch5-sort/quickSort_v1.cpp
+++ lc101/ch5-sort/quickSort_v1.cpp
@@ -3,6 +3,13 @@
 
 using namespace std;
 
+/*
+* 快速排序
+* 思路：选择中轴元素，比它小的放左，比它大的放右(代码过程很像，小丑在扔三个小球)
+* 特点：时间：O(nlogn)、空间：O(logn)、非稳定
+* 适用：广泛(最快)
+*/
+
 void quickSort(vector<int>& a, int low, int high) {
     if (low >= high) // 结束标志
         return;
@@ -15,18 +22,20 @@ void quickSort(vector<int>& a, int low, int high) {
         while (first < last && a[last] > key) {
             last--;
         }
-        if (first < last)
-            a[first++] = a[last];
+        // if (first < last)
+        //     a[first++] = a[last];
         
         // 从前往后走，将比第一个大的移到后面
-        while (first < last && a[first] <= key) {
+        while (first < last && a[first] < key) {
             first++;
         }
-        if (first < last) {
-            a[last--] = a[first];
-        }
+        // if (first < last) {
+        //     a[last--] = a[first];
+        // }
+        swap(a[first], a[last]);
     }
-    a[first] = key;
+    // a[first] = key;
+    swap(a[first], key); 
     // 前半递归
     quickSort(a, low, first-1);
     quickSort(a, first + 1, high);
