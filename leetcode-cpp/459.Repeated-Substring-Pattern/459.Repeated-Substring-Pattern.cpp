b40356641df55ccb47e930c6bec99340270deb48
“yuancf1024” <3455911441@qq.com>
2022-08-02 17:36:12 +0800

    leetcode-459. 重复的子字符串 add 注释

--- leetcode-cpp/459.Repeated-Substring-Pattern/459.Repeated-Substring-Pattern.cpp
+++ leetcode-cpp/459.Repeated-Substring-Pattern/459.Repeated-Substring-Pattern.cpp
@@ -43,24 +43,26 @@ public:
     void getNext(int* next, const string& s) { 
         int j = -1;
         next[0] = -1;
-        for (int i = 1; i < s.size(); i++) {
-            while (j >= 0 && s[i] != s[j+1]) {
-                j = next[j];
+        // j指向前缀末尾位置，i指向后缀末尾位置
+        for (int i = 1; i < s.size(); i++) { // 注意i从1开始
+            while (j >= 0 && s[i] != s[j+1]) { // 前后缀不相同了 // 注意这里是连续回退使用while
+                j = next[j]; // 向前回退
             }
-            if (s[i] == s[j+1]) {
+            if (s[i] == s[j+1]) { // 找到相同的前后缀
                 j++;
             }
-            next[i] = j;
+            next[i] = j; // 将j(前缀的长度)赋给next[i]
         }
     }
 
     bool repeatedSubstringPattern(string s) {
-        if (s.size() == 0) {
+        if (s.size() == 0) { // 处理字符串为空的边界情况
             return false;
         }
         int next[s.size()];
         getNext(next, s);
         int len = s.size();
+        // 检查字符串s是否可以通过由它的一个子串重复多次构成
         if (next[len-1] != -1 && len % (len - (next[len-1] + 1)) == 0) {
             return true;
         }
