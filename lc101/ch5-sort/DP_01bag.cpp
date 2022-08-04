e0e2eaa61e616c5ee1340b9c1904fe1386fe8c3b
“yuancf1024” <3455911441@qq.com>
2022-08-04 11:17:58 +0800

    add 一维DP数组or滚动数组

--- lc101/ch5-sort/DP_01bag.cpp
+++ lc101/ch5-sort/DP_01bag.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+// 二维DP数组
 void test_2_wei_bag_problem1(vector<int>& weight, vector<int>& value, int bagweight) {
     // 二维数组
     vector<vector<int>> dp(weight.size(), vector<int>(bagweight + 1, 0));
@@ -23,12 +24,40 @@ void test_2_wei_bag_problem1(vector<int>& weight, vector<int>& value, int bagwei
         }
     }
     cout << dp[weight.size() - 1][bagweight] << endl;
+
+    // // 遍历dp数组
+    // for (int i = 0; i < dp.size(); i++) {
+    //     for (int j = 0; j < dp[1].size(); j++) 
+    //         cout << dp[i][j] << " " << endl;
+    // }
+}
+
+// 一维DP数组 or 滚动数组
+void test_1_wei_bag_problem(vector<int>& weight, vector<int>& value, int bagweight) {
+    // 初始化
+    vector<int> dp(bagweight + 1, 0);
+
+    // 滚动数组
+    for (int i = 0; i < weight.size(); i++) { // 遍历物品
+        for (int j = bagweight; j >= weight[i]; j--) { // 遍历背包容量
+            dp[j] = max(dp[j], dp[j - weight[i]] + value[i]);
+        }
+    }
+    cout << dp[bagweight] << endl;
+
+    // // 遍历dp数组
+    // for (int i = 0; i < dp.size(); i++) {
+    //     cout << dp[i] << " " << endl;
+    // }
 }
 
 int main() { 
     vector<int> weight = {1, 3, 4};
     vector<int> value = {15, 20, 30};
     int bagweight = 4;
+    cout << "二维DP数组: " << endl;
     test_2_wei_bag_problem1(weight, value, bagweight);
+    cout << "一维DP数组: " << endl;
+    test_1_wei_bag_problem(weight, value, bagweight);
     return 0;
 }
