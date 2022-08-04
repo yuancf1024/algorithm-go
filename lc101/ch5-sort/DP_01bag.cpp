fc3f11c5f67127a0744a8d81cda2c245444fb245
“yuancf1024” <3455911441@qq.com>
2022-08-04 10:57:34 +0800

    01背包 cpp

---
+++ lc101/ch5-sort/DP_01bag.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void test_2_wei_bag_problem1(vector<int>& weight, vector<int>& value, int bagweight) {
+    // 二维数组
+    vector<vector<int>> dp(weight.size(), vector<int>(bagweight + 1, 0));
+
+    // 初始化
+    for (int j = weight[0]; j <= bagweight; j++) {
+        dp[0][j] = value[0];
+    }
+
+    // weight数组的大小，就是物品的个数
+    for (int i = 1; i < weight.size(); i++) { // 遍历物品
+        for (int j = 0; j <= bagweight; j++) { // 遍历背包容量
+            if (j < weight[i]) {
+                dp[i][j] = dp[i - 1][j];
+            } else {
+                dp[i][j] =
+                    max(dp[i - 1][j], dp[i - 1][j - weight[i]] + value[i]);
+            }
+        }
+    }
+    cout << dp[weight.size() - 1][bagweight] << endl;
+}
+
+int main() { 
+    vector<int> weight = {1, 3, 4};
+    vector<int> value = {15, 20, 30};
+    int bagweight = 4;
+    test_2_wei_bag_problem1(weight, value, bagweight);
+    return 0;
+}
