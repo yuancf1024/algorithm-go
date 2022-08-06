96d83eee49e41ca24971dc9a891d80be772344bc
“yuancf1024” <3455911441@qq.com>
2022-08-06 16:11:47 +0800

    meituan move 笔试

---
+++ 笔试/Meituan0423/0423/T5/main.go
@@ -0,0 +1,39 @@
+package main
+
+// import "fmt"
+
+/*
+样例输入
+5 10
+ADD 1 10
+QUERY
+ADD 2 5
+PASS 3
+PASS 2
+QUERY
+PASS 10
+QUERY
+PASS 5
+QUERY
+样例输出
+1
+2
+1
+-1
+*/
+
+// func main() {
+//     var t, n, nums, c int
+// 	log := make([][]int, 0)
+//     fmt.Scan(&t, &n)
+//     for ; n > 0; n-- {
+//         fmt.Scan(&n)
+// 		for ; n > 0; n-- {
+// 			fmt.Scan(&nums)
+// 			nums = append(nums, nums)
+// 		}
+// 		// 核心代码
+// 		// 排序要求：每次交换相邻数，且两个数颜色不能相同
+		
+//     }
+// }
