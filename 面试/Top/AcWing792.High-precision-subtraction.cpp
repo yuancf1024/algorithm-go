4f3f4b144c6e6dbfdbf9ae910e84f7898ba4537d
yuancf1024 <3455911441@qq.com>
2022-08-30 23:54:22 +0800

    补充高精度乘除

--- 面试/Top/AcWing792.High-precision-subtraction.cpp
+++ 面试/Top/AcWing792.High-precision-subtraction.cpp
@@ -37,6 +37,7 @@ using namespace std;
 class Solution {
 public:
     // 实现高精度的减法 单纯的处理正整数 而且结果只能为正
+    // C = A - B, 满足A >= B, A >= 0, B >= 0
     vector<int> sub(vector<int>& A, vector<int>& B) { 
         vector<int> C;
         for (int i = 0, t = 0; i < A.size(); ++i) {
@@ -52,6 +53,65 @@ public:
             }
         }
 
+        while (C.size() > 1 && C.back() == 0) {
+            C.pop_back();
+        }
+        return C;
+    }
+
+    // 高精度加法
+    // C = A + B, A >= 0, B >= 0
+    vector<int> add(vector<int>& A, vector<int>& B) {
+        if (A.size() < B.size()) {
+            return add(B, A); // 确保A的数位数高于B
+        }
+        vector<int> C;
+        int t = 0;
+        for (int i = 0; i < A.size(); ++i) {
+            t += A[i];
+            if (i < B.size()) {
+                t += B[i];
+            }
+            C.push_back(t % 10);
+            t /= 10; // 保留进位结果
+        }
+
+        if (t) { // 保证最后一个进位
+            C.push_back(t);
+        }
+        return C;
+    }
+
+    // 高精度乘低精度
+    // C = A*b, A >= 0, b >= 0
+    vector<int> mul(vector<int>& A, int b) { 
+        vector<int> C;
+
+        int t = 0;
+        for (int i = 0; i < A.size() || t; ++i) {
+            if (i < A.size()) {
+                t += A[i] * b;
+                C.push_back(t % 10);
+                t /= 10;
+            }
+            while (C.size() > 1 && C.back() == 0) {
+                C.pop_back();
+            }
+            return C;
+        }
+    }
+
+    // 高精度除以低精度
+    // A / b = C ... r, A>= 0, b > 0
+    vector<int> div(vector<int>& A, int b, int& r) { 
+        vector<int> C;
+        r = 0;
+        for (int i = A.size() - 1; i >= 0; --i) {
+            r = r * 10 + A[i];
+            C.push_back(r / b);
+            r %= b;
+        }
+        reverse(C.begin(), C.end());
         while (C.size() > 1 && C.back() == 0) {
             C.pop_back();
         }
