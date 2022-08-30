0909627fa491fe62b13ccb9595ce76ddeb01db9f
yuancf1024 <3455911441@qq.com>
2022-08-31 00:05:40 +0800

    补充树节点的构造函数

--- leetcode-cpp/TreeNode.cpp
+++ leetcode-cpp/TreeNode.cpp
@@ -63,7 +63,9 @@ struct TreeNode {
     int val;
     TreeNode *left;
     TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
 };
 
 
