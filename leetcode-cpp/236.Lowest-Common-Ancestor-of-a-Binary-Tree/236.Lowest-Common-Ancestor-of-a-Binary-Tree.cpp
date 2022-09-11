fb84a54b9779250d6c9608c49990b19a70a1616a
yuancf1024 <3455911441@qq.com>
2022-09-11 17:02:17 +0800

    add 巧妙的递归

--- leetcode-cpp/236.Lowest-Common-Ancestor-of-a-Binary-Tree/236.Lowest-Common-Ancestor-of-a-Binary-Tree.cpp
+++ leetcode-cpp/236.Lowest-Common-Ancestor-of-a-Binary-Tree/236.Lowest-Common-Ancestor-of-a-Binary-Tree.cpp
@@ -84,6 +84,25 @@ public:
     }
 };
 
+// 递归的一种非常巧妙的方法
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == nullptr || root == p || root == q) {
+            return root;
+        }
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        if (left == nullptr) {
+            return right;
+        }
+        if (right == nullptr) {
+            return left;
+        }
+        return root;
+    }
+};
+
 /*
 方法一：递归
 思路和算法
