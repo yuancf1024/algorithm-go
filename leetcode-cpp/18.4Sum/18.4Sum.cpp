909ac77b5a71e1763f32c46fc42c7e5fc33e62f9
yuancf1024 <3455911441@qq.com>
2023-07-16 00:03:35 +0800

    leetcode-18.四数之和 排序+双指针+优化 C++

--- leetcode-cpp/18.4Sum/18.4Sum.cpp
+++ leetcode-cpp/18.4Sum/18.4Sum.cpp
@@ -1,6 +1,6 @@
 /* 18. 四数之和
 
-给你一个由 n 个整数组成的数组 nums ，和一个目标值 target 。
+给你一个由n个整数组成的数组nums ，和一个目标值target 。
 请你找出并返回满足下述全部条件且不重复的四元组 
 [nums[a], nums[b], nums[c], nums[d]] 
 （若两个四元组元素一一对应，则认为两个四元组重复）：
@@ -30,6 +30,11 @@
 -10^9 <= target <= 10^9
 
 */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
@@ -119,3 +124,142 @@ target是-10，不能因为-4 > -10而跳过。但是我们依旧可以去做剪
 不用考虑有重复的四个元素相加等于0的情况，
 所以相对于本题还是简单了不少！
 */
+
+
+class Solution2 {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> quadruplets;
+        if (nums.size() < 4) {
+            return quadruplets;
+        }
+        sort(nums.begin(), nums.end());
+        
+        int length = nums.size();
+
+        for (int i = 0 ; i < length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+            if ((long) nums[i] + nums[i+1] + nums[i+2] + nums[i+3] > target) {
+                break;
+            }
+            if ((long) nums[i] + nums[length-3] + nums[length-2] + nums[length-1] < target) {
+                continue;
+            }
+            
+            for (int j = i + 1; j < length - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j-1]) {
+                    continue;
+                }
+
+                if ((long) nums[i] + nums[j] + nums[j+1] + nums[j+2] > target) {
+                    break;
+                }
+
+                if ((long) nums[i] + nums[j] + nums[length-2] + nums[length-1] < target) {
+                    continue;
+                }
+
+                int left = j + 1, right = length - 1;
+                while (left < right) {
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum == target) {
+                        quadruplets.push_back({nums[i], nums[j], nums[left], nums[right]});
+                        while (left < right && nums[left] == nums[left+1]) {
+                            left++;
+                        }
+                        left++;
+                        while (left < right && nums[right] == nums[right-1]) {
+                            right--;
+                        }
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+        return quadruplets;
+    }
+};
+
+int main() {
+    vector<int> nums = {1,0,-1,0,-2,2};
+    int target = 0;
+    vector<vector<int>> res = Solution2().fourSum(nums, target);
+
+    // 打印数组输出
+    for (int i = 0; i < res.size(); i++) {
+        for (int j = 0; j < res[i].size(); j++) {
+            cout << res[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+/**
+ * @brief leetcode官方题解
+ * 
+本题与「15. 三数之和」相似，解法也相似。
+
+方法一：排序 + 双指针
+思路与算法
+
+最朴素的方法是使用四重循环枚举所有的四元组，
+然后使用哈希表进行去重操作，得到不包含重复四元组的
+最终答案。假设数组的长度是n，则该方法中，
+枚举的时间复杂度为O(n^4)，去重操作的时间复杂度和空间复杂度也很高，
+因此需要换一种思路。
+
+为了避免枚举到重复四元组，则需要保证每一重循环枚举
+到的元素不小于其上一重循环枚举到的元素，且在同一重
+循环中不能多次枚举到相同的元素。
+
+为了实现上述要求，可以对数组进行排序，并且在循环过程
+中遵循以下两点：
+
+- 每一种循环枚举到的下标必须大于上一重循环枚举到的下标；
+
+- 同一重循环中，如果当前元素与上一个元素相同，
+    则跳过当前元素。
+
+使用上述方法，可以避免枚举到重复四元组，但是由于
+仍使用四重循环，时间复杂度仍是O(n^4)。注意到数组
+已经被排序，因此可以使用双指针的方法去掉一重循环。
+
+使用两重循环分别枚举前两个数，然后在两重循环枚举
+到的数之后使用双指针枚举剩下的两个数。假设两重循环
+枚举到的前两个数分别位于下标i和j，其中i<j。
+初始时，左右指针分别指向下标j+1和下标n−1。
+每次计算四个数的和，并进行如下操作：
+
+- 如果和等于target，则将枚举到的四个数加到答案中，
+然后将左指针右移直到遇到不同的数，将右指针左移直到遇到不同的数；
+
+- 如果和小于target，则将左指针右移一位；
+
+- 如果和大于target，则将右指针左移一位。
+
+使用双指针枚举剩下的两个数的时间复杂度是O(n)，
+因此总时间复杂度是O(n^3)，低于O(n^4)。
+
+具体实现时，还可以进行一些剪枝操作：
+
+- 在确定第一个数之后，如果 nums[i]+nums[i+1]+nums[i+2]+nums[i+3]>target，
+说明此时剩下的三个数无论取什么值，四数之和一定大于target，因此退出第一重循环；
+
+- 在确定第一个数之后，如果 nums[i]+nums[n−3]+nums[n−2]+nums[n−1]<target，
+说明此时剩下的三个数无论取什么值，四数之和一定小于target，
+因此第一重循环直接进入下一轮，枚举 nums[i+1]；
+
+- 在确定前两个数之后，如果 nums[i]+nums[j]+nums[j+1]+nums[j+2]>target，
+说明此时剩下的两个数无论取什么值，四数之和一定大于target，因此退出第二重循环；
+
+- 在确定前两个数之后，如果 nums[i]+nums[j]+nums[n−2]+nums[n−1]<target，
+说明此时剩下的两个数无论取什么值，四数之和一定小于target，
+因此第二重循环直接进入下一轮，枚举nums[j+1]。
+ */
