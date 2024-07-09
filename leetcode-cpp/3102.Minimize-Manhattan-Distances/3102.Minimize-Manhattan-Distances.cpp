a0d52fe78883014fa6a4431251e0f5a8fc5e7b58
yuancf1024 <3455911441@qq.com>
2024-07-09 23:53:29 +0800

    leetcode-3102.Minimize-Manhattan-Distances 方法2 维护最大次大、最小次小距离

--- leetcode-cpp/3102.Minimize-Manhattan-Distances/3102.Minimize-Manhattan-Distances.cpp
+++ leetcode-cpp/3102.Minimize-Manhattan-Distances/3102.Minimize-Manhattan-Distances.cpp
@@ -40,6 +40,25 @@ points[i].length == 2
 using namespace std;
 
 class Solution {
+    // 更新最大次大
+    void update_max(int v, int& max1, int& max2) {
+        if (v > max1) {
+            max2 = max1;
+            max1 = v;
+        } else if (v > max2) {
+            max2 = v;
+        }
+    }
+    // 更新最小次小
+    void update_min(int v, int& min1, int& min2) {
+        if (v < min1) {
+            min2 = min1;
+            min1 = v;
+        } else if (v < min2) {
+            min2 = v;
+        }
+    }
+
 public:
     // 方法一：有序集合
     int minimumDistance(vector<vector<int>>& points) {
@@ -64,11 +83,36 @@ public:
         }
         return ans;
     }
+
+    // 方法二：维护最大次大、最小次小
+    int minimumDistance_v2(vector<vector<int>>& points) {
+        int max_x1 = INT_MIN, max_x2 = INT_MIN, max_y1 = INT_MIN, max_y2 = INT_MIN;
+        int min_x1 = INT_MAX, min_x2 = INT_MAX, min_y1 = INT_MAX, min_y2 = INT_MAX;
+
+        for (auto& p : points) {
+            int x = p[0] + p[1];
+            int y = p[1] - p[0];
+            update_max(x, max_x1, max_x2);
+            update_min(x, min_x1, min_x2);
+            update_max(y, max_y1, max_y2);
+            update_min(y, min_y1, min_y2);
+        }
+
+        int ans = INT_MAX;
+        for (auto& p : points) {
+            int x = p[0] + p[1];
+            int y = p[1] - p[0];
+            int dx = (x == max_x1 ? max_x2 : max_x1) - (x == min_x1 ? min_x2 : min_x1);
+            int dy = (y == max_y1 ? max_y2 : max_y1) - (y == min_y1 ? min_y2 : min_y1);
+            ans = min(ans, max(dx, dy));
+        }
+        return ans;
+    }
 };
 
 int main() {
     vector<vector<int>> points = {{3, 10}, {5, 15}, {10, 2}, {4, 4}};
-    int res = Solution().minimumDistance(points);
+    int res = Solution().minimumDistance_v2(points);
     cout << res << endl;
 
     return 0;
